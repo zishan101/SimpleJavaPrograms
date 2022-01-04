4733330d1dd690d736e7a62299bcca7d3af73bdb
zishan101 <zishan101@gmail.com>
2022-01-04 11:35:54 +0530

    Committed Code

---
+++ TestProject1/src/TestPackage/TestHello.java
@@ -0,0 +1,12 @@
+package TestPackage;
+
+public class TestHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing Hello World ");
+
+	}
+
+}
